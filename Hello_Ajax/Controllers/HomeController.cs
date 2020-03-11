6de44e3b4bea32f9a6c96208ba23be1db681c822
essarraj.fouad <essarraj.fouad@gmail.com>
2020-03-11 08:55:01 +0100

    Ajoutez des fichiers projet.

---
+++ Hello_Ajax/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hello_Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Page1()
+        {
+
+            return View();
+        }
+
+        public ActionResult Somme(float x, float y)
+        {
+            float z = x + y;
+            ViewBag.result = z;
+            return View();
+        }
+    }
+}
---
+++ Hello_Ajax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hello_Ajax.MvcApplication" Language="C#" %>
